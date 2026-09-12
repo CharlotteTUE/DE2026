3fbee0998284bd91e2009d5c23c50eb16aed30ee
Indika Kumara <ipkumarawd@yahoo.com>
2026-09-12 20:13:21 +0200

    Documented sh files

--- lab0/flask-helloworld/run_app.sh
+++ lab0/flask-helloworld/run_app.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# Specify that the interpreter is bash
 
+# Run apt command in privileged mode to install Python dev tools and the virtual environment module
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install python3-dev python3-pip python3-venv -y
- # Only create a virtual environment if it does not exist
+ # Only create a virtual environment (directory) if it does not exist
 if [ ! -d ".venv" ]; then
   python3 -m venv .venv
 fi
